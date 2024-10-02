826ce83e124757baf6394db2d3859ada7e40a67d
Wenderson Aquino <wensantos.aquino@gmail.com>
2024-10-02 18:43:17 -0300

    Add files via upload

---
+++ SistemaElevador/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevador elevador = new Elevador(6, 12);
+            elevador.Entrar(3);
+            elevador.Subir(2);
+            elevador.Descer(1);
+            elevador.Deslocar(5);
+            elevador.Sair(2);
+            Console.ReadKey();
+        }
+    }
+}
